2cc6ce5db924f3b7fdef81bd160d25dd5f3201be
br1battaglia <br1.battaglia@mail.com>
2021-03-21 05:00:24 +0100

    comment deleted

--- simulator.py
+++ simulator.py
@@ -1,4 +1,3 @@
-from warnings import catch_warnings
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
@@ -43,6 +42,8 @@ def init(DG):
 	doubleOutputNode = []
 	singleInputNode = []
 	doubleInputNode = []
+	for i in range(len(inDegreeNode)):
+		inDegreeNode[i][1] = 3
 	
 	ghostInputNode = random.sample(inDegreeNode, (int(0.26*len(node))))
 	inDegreeNode = diff(inDegreeNode, ghostInputNode)
@@ -50,8 +51,6 @@ def init(DG):
 	inDegreeNode = diff(inDegreeNode, singleInputNode)
 	doubleInputNode = random.sample(inDegreeNode, (int(0.05*len(node))))
 	inDegreeNode = diff(inDegreeNode, doubleInputNode)
-	for i in range(len(inDegreeNode)):
-		inDegreeNode[i][1] = 3
 
 	ghostOutputNode = random.sample(outDegreeNode, (int(0.22*len(node))))
 	outDegreeNode = diff(outDegreeNode, ghostOutputNode)
@@ -78,20 +77,16 @@ def chooseSender(singleOutputNode, doubleOutputNode, multiOutputNode):
 	index = None
 	currentList = None
 	randomOutput = random.randrange(1,4)	
-	print("Random Output: "+str(randomOutput))
 	if(randomOutput == 1 and len(singleOutputNode) !=0):
 		currentList = singleOutputNode
-		print("Cambio sender")
 		sender = random.choice(currentList)
 		index = 1	
 	elif(randomOutput == 2 and len(doubleOutputNode) !=0):
 		currentList = doubleOutputNode
-		print("Cambio sender")
 		sender = random.choice(currentList)
 		index = 2
 	elif(randomOutput == 3 and len(multiOutputNode) !=0):
 		currentList = multiOutputNode
-		print("Cambio sender")
 		sender = random.choice(currentList)
 		index = random.randrange(3,9)
 	else:
@@ -100,9 +95,11 @@ def chooseSender(singleOutputNode, doubleOutputNode, multiOutputNode):
 
 def fillGraph(DG):
 	x = init(DG)
+	#ghostInputNode = x[0]
 	singleInputNode = x[1]
 	doubleInputNode = x[2]
 	multiInputNode = x[3]
+	#ghostOutputNode = x[4]
 	singleOutputNode = x[5]
 	doubleOutputNode = x[6]
 	multiOutputNode = x[7]
@@ -115,34 +112,22 @@ def fillGraph(DG):
 		sender = data[0]
 		index = data[1]
 		currentList = data[2]
-		""" print("Index: "+str(index))
-		print("Sender: "+str(sender)) """
 		for i in range(index):
 			while(True):
 				receiver = chooseReceiver(singleInputNode, doubleInputNode, multiInputNode)
 				if(receiver != 0 and receiver != sender):
 					break
-			print("Receiver: "+str(receiver))
 			addTransaction(DG, sender,receiver)
 			receiver[1] = receiver[1]+ 1
 			if(receiver in singleInputNode and receiver[1] == 1):
 				singleInputNode.remove(receiver)
 			elif(receiver in doubleInputNode and receiver[1] == 2):
 				doubleInputNode.remove(receiver)
-		""" print("Seize sON: "+str(len(singleOutputNode)))
-		print("Seize dON: "+str(len(doubleOutputNode)))
-		print("Seize mON: "+str(len(multiOutputNode)))
-		print("Seize sIN: "+str(len(singleInputNode)))
-		print("Seize dIN: "+str(len(doubleInputNode)))
-		print("Seize mON: "+str(len(multiOutputNode))) """
 		try:
 			currentList.remove(sender)
 		except:
 			pass
 		if(len(singleOutputNode) == 0 and len(doubleOutputNode) == 0 and len(multiOutputNode) == 0):
-			print("Seize sIN: "+str(len(singleInputNode)))
-			print("Seize dIN: "+str(len(doubleInputNode)))
-			print("Seize mIN: "+str(len(multiInputNode))) 
 			break
 
 
@@ -153,51 +138,3 @@ fillGraph(DG)
 nx.draw_networkx(DG)
 nx.write_gml(DG, "bitcoin.gml")
 plt.show()
-
-
-
-
-
-
-
-""" while(True):
-	enter = True
-	x = random.choice(outDegreeNode)
-	if(len(singleInputNode) < int(0.25*len(node))):
-		singleOutputNode.append(x)
-		outDegreeNode.remove(x)
-		enter = False
-	if(len(singleOutputNode) >= int(0.25*len(node)) and len(doubleOutputNode) < int(0.5*len(node)) and enter == True):
-		doubleOutputNode.append(x)	
-		outDegreeNode.remove(x)
-		enter = False
-	if(len(singleOutputNode) >= int(0.25*len(node)) and len(doubleOutputNode) >= int(0.5*len(node)) and len(multiOutputNode) < int(0.03*len(node)) and enter == True):
-		multiOutputNode.append(x)
-		outDegreeNode.remove(x)
-		if(len(multiOutputNode) >= int(0.03*len(node))):
-			break """
-	
-
-
-""" randomOutput = random.randrange(1,3)
-		
-	if randomInput == 1:
-		currentInputList = singleInputNode
-	elif randomInput == 2:
-		currentInputList = doubleInputNode 
-	 elif randomInput == 3:
-		currentInputList = multiInputNode 
-
-	randomOutput = random.randrange(1,3)
-	if randomOutput == 1:
-		currentOutputList = singleOutputNode
-	elif randomOutput == 2:
-		currentInOutputList = doubleOutputNode
-	elif randomOutput == 2:
-		currentOutputList = multiOutputNode 
-	
-	while(True):
-		sender = random.choice(currentOutputList)
-		receiver = random.choice(currentInputList)
-		if(sender != receiver):
-			break """
