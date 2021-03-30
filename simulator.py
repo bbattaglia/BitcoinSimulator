b58e2a10405608184e7059c1174027f49596f85f
br1battaglia <br1.battaglia@mail.com>
2021-03-30 17:00:37 +0200

    metrix file

--- simulator.py
+++ simulator.py
@@ -1,10 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-import powerlaw
+from metrix import *
 
-
-from networkx.algorithms.centrality.degree_alg import in_degree_centrality
 
 def diff(list1, list2):
 	for i in range(len(list2)):
@@ -131,73 +129,6 @@ def fillGraph(DG):
 		if(len(singleOutputNode) == 0 and len(doubleOutputNode) == 0 and len(multiOutputNode) == 0) :
 			break
 
-def calculateAverageDegree(G):
-	sum = 0
-	averageDegree = 0
-	averageDegree = nx.average_degree_connectivity(G)
-	for value in averageDegree:
-		sum = sum + averageDegree[value]
-	averageDegree = sum/len(averageDegree)
-	return averageDegree
-
-
-def calculateMetrix(DG):
-
-	result = powerlaw.Fit(list(DG))
-	print("alpha; "+str(result.alpha))
-
-
-	sumOut = sumIn = singleIn = singleOut = doubleIn = doubleOut = zeroIn = zeroOut = totOut = totIn = 0
-	for i in range(len(DG)):
-		#in degree 
-		totOut = totOut + DG.out_degree(i)
-		if DG.out_degree(i) ==1 :
-			singleOut = singleOut+1
-		elif DG.out_degree(i) == 2:
-			doubleOut = doubleOut+1
-		elif DG.out_degree(i) == 0:
-			zeroOut = zeroOut+1
-		elif DG.out_degree(i) >2:
-			sumOut = sumOut+1
-		#out degree
-		totIn = totIn + DG.in_degree(i)
-		if DG.in_degree(i) == 1 :
-			singleIn = singleIn+1
-		elif DG.in_degree(i) == 2:
-			doubleIn = doubleIn+1
-		elif DG.in_degree(i) == 0:
-			zeroIn = zeroIn+1
-		elif DG.in_degree(i) >2:
-			sumIn = sumIn+1 
-	print("Transazioni per nodo:")
-	print("OutDregree (#numtransaction, #node): zero: "+str(zeroOut)+", single: "+str(singleOut)+", double: "+str(doubleOut)+". multi: "+str(sumOut))
-	print("InDegree (#numtransaction, #node): "+str(zeroIn)+", single: "+str(singleIn)+", double: "+str(doubleIn)+". multi: "+str(sumIn))
-	print("InDegree medio: "+str(totIn/len(list(DG))))
-	print("OutDegree medio: "+str(totOut/len(list(DG)))) 
-	print("--------------")
- 
-	#degree medio main component
-	G = nx.to_undirected(DG)
-	mainComponents = sorted(nx.connected_components(G), key=len, reverse=True)
-	MC = G.subgraph(mainComponents[0])
-	print("Net average degree: "+str(calculateAverageDegree(DG)))
-	print("MC average degree: "+str(calculateAverageDegree(MC)))
-	print("--------------")
-
-	#average clustering coefficient
-	
-	print("ACC MC: "+str(nx.average_clustering(MC)))
-	print("--------------")
-	
-	print("ASPL MC: "+str(nx.average_shortest_path_length(MC)))
-	S = [DG.subgraph(c).copy() for c in nx.weakly_connected_components(DG)]
-	for index,value in enumerate(S):
-		try: aspl
-		except NameError: aspl = 0
-		aspl = aspl + nx.average_shortest_path_length(value)
-		if index == (len(S)-1):
-			aspl = aspl/len(S)
-	print("ASPL: "+str(aspl))  
 
 
 
